f8418d21a5875739e47aaba9e365c3ef5a88a941
Noe Javet <javetnoe@students.zhaw.ch>
2024-06-05 15:20:49 +0200

    start with implementing numerical methods and writing summaries

---
+++ numerical_mathematics/computer_arithmetics.md
@@ -0,0 +1,47 @@
+
+## Infinity
+The set of real numbers is infinite, uncountable, and therefore it is 
+impossible to represent all numbers on a finite machine (humans would 
+also count as finite machine in my opinion, since I never saw somebody 
+computing with "real" real numbers).
+For a Basis B ∈ ℕ with B > 1 we can express any real number x ∈ ℝ like this:
+
+x = m * B^e (exponent e ∈ ℤ, m ∈ ℝ)
+
+### Floating point numbers
+The set M of machine representable numbers to the Basis B is:
+
+M = {x ∈ ℝ | x = +/- 0.m1m2m3...mn * B ^ (+/- e1e2..el) ∪ {0}
+
+
+
+real numbers: <Ctrl>-v u211d
+natural numbers u2115
+numbers: u2124
+
+unicode inserts:
+in insert mode, press <Ctrl>-v then press u<unicode string>
+
+negation ¬ : <Ctrl>-v u00ac
+implication → : <Ctrl>-v u2192
+conjunction ∧ : <Ctrl>-v u2227
+disjunction ∨ : <Ctrl>-v u2228
+bi-implication ↔ : <Ctrl>-v u2194
+
+syntactic identity:
+equivalence / identity ≡: <Ctrl>-v u2261
+
+equivalnce ⇔ : <Ctrl>-v u21d4 
+
+All quantor ∀ : <Ctrl>-v u2200
+Existence quantor ∃ : <Ctrl>-v u2203
+element of ∈ : <Ctrl>-v u2208
+subset of ⊆ : <Ctrl>-v u2286
+union ∪: <Ctrl>-v u222a
+intersection ∩ : <Ctrl>-v u2229
+
+true ⊤ : <Ctrl>-v u22a4
+falsity ⊥: <Ctrl>-v u22a5
+entailment ⊨ : <Ctrl>-v u22a8
+inference  ⊢ : <Ctrl>-v u22a2
+
---
+++ tests/test_linear_system_solver.py
@@ -0,0 +1,51 @@
+import unittest
+import numpy as np
+
+
+class Gauss(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_3x3_matrix_0(self):
+        A = np.array([1, 5, 6, 7, 9, 6, 2, 3, 4], dtype=np.float64).reshape(3, 3)
+        b = np.array([29, 43, 20], dtype=np.float64)
+
+    def test_3x3_matrix_1(self):
+        A = np.array([-1, 1, 1, 1, -3, -2, 5, 1, 4],
+                     dtype=np.float64).reshape(3, 3)
+        b = np.array([0, 5, 3], dtype=np.float64)
+
+    def test_3x3_matrix_2(self):
+        A = np.array([4, -1, -5, -12, 4, 17, 32, -10, -41], 
+                     dtype=np.float64).reshape(3, 3)
+        b0 = np.array([-5, 19, -39], dtype=np.float64)
+        b1 = np.array([6, -12, 48], dtype=np.float64)
+
+    def test_3x3_matrix_3(self):
+        A = np.array([2, 7, 3, -4, -10, 0, 12, 34, 9],
+                     dtype=np.float64).reshape(3, 3)
+        b0 = np.array([25, -24, 107], dtype=np.float64)
+        b1 = np.array([5, -22, 42], dtype=np.float64)
+
+    def test_3x3_matrix_4(self):
+        A = np.array([-2, 5, 4, -14, 38, 22, 6, -9, -27],
+                     dtype=np.float64).reshape(3, 3)
+        b0 = np.array([1, 40, 75], dtype=np.float64)
+        b1 = np.array([16, 82, -120], dtype=np.float64)
+
+    def test_8x8_matrix_0(self):
+        A = np.array([[-1, 2, 3, 2, 5, 4, 3, -1],
+                      [3, 4, 2, 1, 0, 2, 3, 8],
+                      [2, 7, 5, -1, 2, 1, 3, 5],
+                      [3, 1, 2, 6, -3, 7, 2, -2],
+                      [5, 2, 0, 8, 7, 6, 1, 3],
+                      [-1, 3, 2, 3, 5, 3, 1, 4],
+                      [8, 7, 3, 6, 4, 9, 7, 9],
+                      [-3, 14, -2, 1, 0, -2, 10, 5]],
+                     dtype=np.float64)
+        b = np.array([-11, 103, 53, -20, 95, 78, 131, -26], dtype=np.float64)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
