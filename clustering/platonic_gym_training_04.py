6fb02a6278d16ae287c3d851c69071393dddf96e
Noe Javet <javetnoe@students.zhaw.ch>
2024-06-07 21:19:03 +0200

    add sklearn dbscan for comparision

--- clustering/platonic_gym_training_04.py
+++ clustering/platonic_gym_training_04.py
@@ -3,18 +3,19 @@ dbscan training
 """
 
 from sklearn.datasets import make_blobs
+from sklearn.cluster import DBSCAN
 import matplotlib.pyplot as plt
 import collections
 from enum import Enum
 import numpy as np
 
-X, _ = make_blobs(n_samples=16,
+X, _ = make_blobs(n_samples=300,
                   n_features=2,
-                  centers=2,
-                  cluster_std=0.3,
+                  centers=4,
+                  cluster_std=1,
                   random_state=0x101)
 
-fig = plt.figure(figsize=(16, 16))
+fig = plt.figure(figsize=(32, 16))
 
 # hyperparameters
 eps = 0.5
@@ -77,22 +78,40 @@ for i, p in enumerate(X):
 
 colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(clusters))]
 # colors = ['cyan', 'purple', 'pink', 'green']
-ax = fig.add_subplot(111)
+ax = fig.add_subplot(121)
 ax.scatter(X[noise_points, 0], X[noise_points, 1], color='black')
 for point in X[noise_points]:
     circle = plt.Circle(point, eps, color='black', fill=False, linestyle='--')
     plt.gca().add_patch(circle)
 for point in X[core_points]:
     circle = plt.Circle(point, eps, color='red', fill=False)
-    plt.gca().add_patch(circle)
+    #plt.gca().add_patch(circle)
 for point in X[border_points]:
     circle = plt.Circle(point, eps, color='green', fill=False)
-    plt.gca().add_patch(circle)
+    #plt.gca().add_patch(circle)
 
 for i, lst in clusters.items():
     print(f'cluster = {i}, points: {lst}')
-    ax.scatter(X[lst, 0], X[lst, 1], color=colors[i])
+    ax.scatter(X[lst, 0], X[lst, 1], color=colors[i], label=str(i))
 
+ax = fig.add_subplot(122)
+dbscan = DBSCAN(min_samples=4, eps=0.5)
+dbscan.fit(X)
+
+for k, col in zip(set(dbscan.labels_), colors):
+    xy = X[dbscan.labels_ == k]
+    if k == -1:
+        print('yo')
+        for point in xy:
+            circle = plt.Circle(point, eps, color=col, fill=False)
+            plt.gca().add_patch(circle)
+        col = [0, 0, 0, 1]
+
+    ax.scatter(xy[:, 0], xy[:, 1], c=[col], label='scipy' + str(k))
+
+
+
+plt.legend()
 plt.show()
 
 
