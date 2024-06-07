23ba17bbb240a0902c04b64c5ff25991771fb158
Noe Javet <javetnoe@students.zhaw.ch>
2024-06-07 16:17:21 +0200

    centroid assignment not clear, zikzak style with max distance

--- clustering/t03.py
+++ clustering/t03.py
@@ -8,39 +8,41 @@ import collections
 import matplotlib.pyplot as plt
 
 
-X, _ = make_blobs(n_samples=16,
-                  n_features=2,
+X, _ = make_blobs(n_samples=300,
+                  n_features=4,
                   cluster_std=0.8,
-                  centers=2,
+                  centers=4,
                   random_state=0x101)
 
-K = 2
+K = 4
 fig = plt.figure(figsize=(32, 16))
 
 colors = ['cyan', 'purple', 'orange', 'green']
 # centroids initialization
-centroids = []
 index = np.random.choice(len(X))
+centroid_inds = [index]
 centroids = [X[index]]
 for centroid_ind in range(K-1):
     prev_centroid = centroids[-1]
     distances = []
     for i, data_point in enumerate(X):
         dist = np.linalg.norm(data_point - prev_centroid, 2)
-        distances.append((dist, i))
-    _, p_ind = sorted(distances, reverse=True)[0]
+        if i not in centroid_inds:
+            distances.append((dist, i))
+
+    dis = sorted(distances, reverse=True)[:8]
+    _, p_ind = dis[np.random.choice(8)]
+
+
     centroids.append(X[p_ind])
+    centroid_inds.append(p_ind)
 centroids = np.array(centroids)
 
 
 for n_iter in range(8):
+    old_centroids = np.copy(centroids)
     ax = fig.add_subplot(2, 4, n_iter + 1)
     ax.grid()
-    ax.scatter(centroids[:, 0], 
-               centroids[:, 1], 
-               c='red', 
-               label='centroids',
-               zorder=5)
 
     c2p = collections.defaultdict(list)
     for i, p in enumerate(X):
@@ -59,6 +61,14 @@ for n_iter in range(8):
                    color=colors[ci],
                    label=str(ci))
         centroids[ci] = np.mean(data_points, axis=0)
+
+    ax.scatter(old_centroids[:, 0], 
+               old_centroids[:, 1], 
+               c='black', 
+               label='centroids',
+               linewidths=3,
+               marker='x',
+               s=200)
 
 
 plt.legend()
