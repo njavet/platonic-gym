3c7c218d324787327e6efd052588f88fbb39e005
Noe Javet <javetnoe@students.zhaw.ch>
2024-06-08 16:25:58 +0200

    1-d output neural net

--- neural_networks/snn.py
+++ neural_networks/snn.py
@@ -4,103 +4,85 @@ understanding deep learning book, shallow neural networks implementation
 """
 from typing import Callable
 import numpy as np
-import sympy as sp
-
-
-class Neuron:
-    def __init__(self,
-                 input_dim: int,
-                 activation: Callable,
-                 random_seed: int = 0x101):
-        self.input_dim = input_dim
-        self.activation = activation
-        self.rng = np.random.default_rng(random_seed)
-        self.bias = self.rng.standard_normal()
-        self.weights = self.rng.standard_normal(self.input_dim)
-
-    def forward(self, inputs):
-        net_sum = self.bias + np.dot(inputs, self.weights)
-        return self.activation(net_sum)
-
-    def predict(self, x):
-        pass
-
-    def fit(self, inputs, targets, alpha=0.1, epochs=512):
-        for _ in range(epochs):
-            for x, y in zip(inputs, targets):
-                err = self.forward(x) - y
-                self.bias -= alpha * err
-                self.weights -= alpha * err * x
 
 
 class SNN:
+    """
+    only 1-d outputs for now
+    """
     def __init__(self,
                  d_layer: int,
                  d_in: int,
                  d_out: int,
                  act: Callable,
                  der_act: Callable,
-                 random_seed: int = 0x101):
+                 seed: int = 0x101):
         self.d_layer = d_layer
         self.d_in = d_in
         self.d_out = d_out
         self.act = act
         self.der_act = der_act
-        self.beta0: np.ndarray
-        self.beta1: np.ndarray
-        self.omega0: np.ndarray
-        self.omega1: np.ndarray
-        self.init_network(random_seed)
+        self.beta0: np.ndarray | None = None
+        self.beta1: np.ndarray | None = None
+        self.omega0: np.ndarray | None = None
+        self.omega1: np.ndarray | None = None
+        self.init_network(seed)
 
-    def init_network(self, random_seed):
-        rng = np.random.default_rng(random_seed)
+    def init_network(self, seed):
+        rng = np.random.default_rng(seed)
         self.beta0 = rng.standard_normal(self.d_layer)
-        self.beta1 = rng.standard_normal(self.d_out)
+        self.beta1 = np.random.rand()
         self.omega0 = rng.standard_normal(self.d_layer * self.d_in).reshape(
             (self.d_layer, self.d_in)
         )
-        self.omega1 = rng.standard_normal(self.d_layer * self.d_out).reshape(
-            (self.d_out, self.d_layer)
-        )
+        self.omega1 = rng.standard_normal(self.d_layer * self.d_out)
 
-    def forward(self, inputs: np.ndarray):
-        h1 = self.act(self.beta0 + np.dot(self.omega0, inputs))
+    def forward(self, x: np.ndarray):
+        h1 = self.act(self.beta0 + np.dot(self.omega0, x))
         return self.beta1 + np.dot(self.omega1, h1)
 
     def predict(self, inputs):
-        pass
-
-    def compute_grad_beta0(self, pred, x, y):
-        grad_b0 = np.zeros_like(self.beta0)
-        for i, b in enumerate(self.beta0):
-            tmp = np.dot(self.omega0[i], x) + b
-            grad_b0[i] = 2 * (pred - y) * self.der_act(tmp)
-        return grad_b0
-
-    def compute_grad_beta1(self, pred, y):
-        return np.full(self.d_out, 2 * (pred - y))
-
-    def compute_grad_omega0(self, pred, x, y):
-        grad_o0 = np.zeros_like(self.omega0)
-        for i, w in self.omega0:
-            tmp = self.beta0[i] + np.dot()
-            grad_o0[i] = 2 * (pred - y) * self.der_act(tmp)
-
-    def compute_grad_omega1(self, pred, x, y):
-        grad_o1 = np.zeros_like(self.omega1)
-        for i, w in self.omega1:
-            tmp = self.beta0[i] + np.dot(self.omega0[:, i], x)
-            grad_o1[i] = 2 * (pred - y) * self.act(tmp)
-        return grad_o1
+        y = np.zeros(inputs.shape[0])
+        for i, x in enumerate(inputs):
+            y[i] = self.forward(x)
+        return y
 
     def fit(self, inputs, targets, alpha=0.01, epochs=512):
         for _ in range(epochs):
 
             for x, y in zip(inputs, targets):
-                pred = self.forward(x)
-                grad_b1 = self.compute_grad_beta1(pred, y)
-                grad_b0 = self.compute_grad_beta0(pred, x, y)
-                grad_o0 = self.compute_grad_omega0(pred, x, y)
-                grad_o1 = self.compute_grad_omega1(pred, x, y)
+                # column vectors
+                part0 = 2 * (self.forward(x) - y)
+                tmp = self.beta0 + np.dot(self.omega0, x)
+                # a simple float
+                self.beta1 -= alpha * part0
+                # d-in array
+                self.beta0 -= alpha * part0 * np.dot(self.omega1, self.der_act(tmp))
+                self.omega1 -= alpha * part0 * self.act(tmp)
+                part1 = np.dot(self.omega1, self.der_act(tmp))
+                self.omega0 -= alpha * part0 * part1 * x
 
 
+def relu(x):
+    return np.maximum(0, x)
+
+
+def relu_derivative(x):
+    return (x > 0.).astype(float)
+
+
+if __name__ == '__main__':
+    inputs = np.array([[0., 0.],
+                       [0., 1.],
+                       [1., 0.],
+                       [1., 1.]])
+    targets = np.array([0., 1., 1., 0.])
+
+    snn = SNN(d_layer=3,
+              d_in=2,
+              d_out=1,
+              act=relu,
+              der_act=relu_derivative)
+
+    snn.fit(inputs, targets)
+    print(snn.predict(inputs))
--- state.md
+++ state.md
@@ -1,3 +1,12 @@
 
 ## Machine Learning
-* inear Regression
+* Linear Regression 
+* Polynomial Regression
+* Logistic Regression
+* SVM
+* Decision Trees
+* Neural Networks
+* Clustering 
+ 
+how to design loss functions (RSS for regression, log loss for classification)
+
