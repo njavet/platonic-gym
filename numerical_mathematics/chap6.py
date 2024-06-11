c1359950fbe5b1c4ad6250aa1e12bca7175d897d
Noe Javet <javetnoe@students.zhaw.ch>
2024-06-11 12:19:20 +0200

    add lagrange implementation

--- numerical_mathematics/chap6.py
+++ numerical_mathematics/chap6.py
@@ -20,12 +20,48 @@ def lagrange_basis_li(i, x, x_points):
         if i != j:
             li *= (x - x_points[j]) / (x_points[i] - x_points[j])
     return li
+
+
+class LagrangePoly:
+    def __init__(self, x_points, y_points):
+        sp.init_printing()
+        assert len(x_points) == len(y_points)
+        self.n = len(x_points) 
+        self.x_points = x_points
+        self.y_points = y_points
+        self.sp_poly = 0
+        self.sp_vector = np.zeros(self.n)
+        self.f_poly = self.construct_sym_poly()
+
+    def construct_sym_poly(self):
+        x = sp.symbols('x')
+        self.sp_poly = 0
+        self.sp_vector = np.zeros(self.n)
+        for i in range(self.n):
+            li = sp.simplify(self.li(i, x) * self.y_points[i])
+            self.sp_poly += li
+        return sp.lambdify(x, self.sp_poly, 'numpy')
+
+    def li(self, i, x):
+        res = 1
+        for j in range(self.n):
+            if i != j:
+                res *= (x - self.x_points[j]) / (self.x_points[i] - self.x_points[j])
+        return res
+
+    def eval_poly(self, x):
+        p = 0 
+        for i in range(self.n):
+            p += self.li(i, x) * self.y_points[i]
+        return p
     
 
 
 xs = np.array([8., 10., 12., 14.])
-
 ys = np.array([11.2, 13.4, 15.3, 19.5])
-print(lagrange_polynomial(11, xs, ys))
-
+lp = LagrangePoly(xs, ys)
+sp.pprint(lp.sp_poly)
+print(lp.sp_vector)
+print(lp.f_poly(11))
+print(lp.eval_poly(11))
 
