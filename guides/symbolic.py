3d942015a53feb4487a87e02b04bb23c12533c7b
Noe Javet <javetnoe@students.zhaw.ch>
2024-06-11 10:24:10 +0200

    add newton method

--- guides/symbolic.py
+++
@@ -1,5 +0,0 @@
-import sympy as sp
-import numpy as np
-import matplotlib.pyplot as plt
-
-
--- numerical_mathematics/chap4.py
+++ numerical_mathematics/chap4.py
@@ -143,3 +143,19 @@ if __name__ == '__main__':
                                number=1)
         print(f'n = {n}, flops = {flo[n]}, {e_time:.4f} sec')
 
+import numpy as np
+
+
+def jacobi(A, b, x0):
+    L = np.tril(A)
+    np.fill_diagonal(L, 0)
+    Dinv = np.diag(1 / np.diag(A))
+    R = np.triu(A)
+    np.fill_diagonal(R, 0)
+
+    for _ in range(5):
+        xn = -np.dot(Dinv, np.dot(L + R, x0)) + np.dot(Dinv, b)
+        x0 = xn
+        print(f'{xn}')
+    return xn
+
